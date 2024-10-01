59927d23f69665a8cf78e66ee30493c6f784da39
Enin Dmitriy <d.enin@innopolis.university>
2024-10-01 13:47:52 +0300

    Deployment with docker

---
+++ code/datasets/preprocess.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def load_split(path):
+    df = pd.read_csv(path)
+    df.dropna(inplace=True)
+
+    X = df['Message']
+    y = df['Class']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    return X_train, X_test, y_train, y_test
+
+
+if __name__ == '__main__':
+    X_train, X_test, y_train, y_test = load_split('../../data/raw/Spam_SMS.csv')
+    X_train.to_csv('../../data/processed/X_train.csv', index=False)
+    X_test.to_csv('../../data/processed/X_test.csv', index=False)
+    y_train.to_csv('../../data/processed/y_train.csv', index=False)
+    y_test.to_csv('../../data/processed/y_test.csv', index=False)
